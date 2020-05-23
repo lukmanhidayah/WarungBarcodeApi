97470f04709fe5829c9cfc4f635fd776828a846b
lukmanhidayah01@gmail.com <lukmanhidayah01@gmail.com>
2020-05-23 11:59:00 +0700

    update produk

--- src/api/controllers/produk.js
+++ src/api/controllers/produk.js
@@ -1,34 +1,45 @@
 const connection = require('../../config/db')
 
 exports.get_all_produk = (req, res, next) => {
-    connection.query('SELECT * FROM produk', function (error, rows, fields) {
-        if (error) {
-            res.status(500).json({ error: error })
-        } else {
 
-            const response = {
-                status: true,
-                message: "Data Produk Ditemukan",
-                total: rows.length,
-                data: rows.map(row => {
-                    return {
-                        id: row.id,
-                        barcode: row.barcode,
-                        nama: row.nama,
-                        harga: row.harga
-                    }
-                })
-            }
-            if (rows.length > 0) {
-                res.status(200).json(response)
+    if (req.query.page == undefined || req.query.page == "") {
+        return res.status(200).json({
+            status: false,
+            message: "URL tidak lengkap",
+        })
+    } else {
+        connection.query('SELECT * FROM produk', function (error, rows, fields) {
+            if (error) {
+                res.status(500).json({ error: error })
             } else {
-                res.status(200).json({
-                    status: false,
-                    message: "Data Produk Tidak Ditemukan",
-                    data: []
-                })
-            }
-        }
 
-    });
+                const response = {
+                    status: true,
+                    message: "Data Produk Ditemukan",
+                    total: rows.length,
+                    data: rows.map(row => {
+                        return {
+                            id: row.id,
+                            barcode: row.barcode,
+                            nama: row.nama,
+                            harga: row.harga,
+                            img: row.img
+                        }
+                    })
+                }
+                if (rows.length > 0) {
+                    res.status(200).json(response)
+                } else {
+                    res.status(200).json({
+                        status: false,
+                        message: "Data Produk Tidak Ditemukan",
+                        data: []
+                    })
+                }
+            }
+
+        });
+    }
+
+
 };
